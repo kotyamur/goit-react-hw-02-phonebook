b39b9bcd0bbbb9bdbddd848d569cd4599d587265
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-15 22:39:15 +0300

    add filter completed 3 step

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
   changeInput = e => {
@@ -36,8 +42,22 @@ export class App extends Component {
     this.setState({ name: '', number: '' });
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getContactsByName = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
-    const { contacts, name, number } = this.state;
+    const { name, number, filter } = this.state;
+    const filteredContacts = this.getContactsByName();
     return (
       <div>
         <h2>Phonebook</h2>
@@ -69,8 +89,13 @@ export class App extends Component {
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+        <label>
+          Find contacts by name
+          <input value={filter} onChange={this.changeFilter} />
+        </label>
+
         <ul>
-          {contacts.map(contact => (
+          {filteredContacts.map(contact => (
             <li key={contact.id}>
               {contact.name}: {contact.number}
             </li>
