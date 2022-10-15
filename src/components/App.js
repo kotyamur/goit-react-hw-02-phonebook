be7aca3a2afcbe2ee9971712554dbca7ed98521c
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-15 21:03:40 +0300

    add metods changeInput and addContact

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
@@ -6,19 +7,47 @@ export class App extends Component {
     name: '',
   };
 
+  changeInput = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  addContact = name => {
+    const contact = {
+      id: nanoid(),
+      name,
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
   render() {
+    const { contacts, name } = this.state;
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        React homework template
+      <div>
+        <h2>Phonebook</h2>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.changeInput}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
