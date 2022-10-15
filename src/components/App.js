596714e890441679dd6ce1c261f903b9aa9ad1f4
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-15 20:13:14 +0300

    create class App

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
