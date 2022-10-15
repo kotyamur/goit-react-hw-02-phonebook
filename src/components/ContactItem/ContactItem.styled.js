bb11e910e46144c64cfa944a1bc6c82c4d092d7f
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-16 02:29:30 +0300

    fix styles

--- src/components/ContactItem/ContactItem.styled.js
+++ src/components/ContactItem/ContactItem.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.ul`
+export const Container = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
